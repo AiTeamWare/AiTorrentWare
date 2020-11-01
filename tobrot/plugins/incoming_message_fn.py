12879d593ddbfc2de7c5219eee13347cf6f5b695
TMAdminz <71258154+TMAdminz@users.noreply.github.com>
2020-11-01 16:04:44 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -13,6 +13,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 import os
+import requests
 
 from tobrot import (
     DOWNLOAD_LOCATION
@@ -28,7 +29,9 @@ from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
 from tobrot.helper_funcs.admin_check import AdminCheck
-        
+from tobrot.helper_funcs.ytplaylist import yt_playlist_downg
+from tobrot.helper_funcs.cloneHelper import CloneHelper
+
 async def incoming_purge_message_f(client, message):
     """/purge command"""
     i_m_sefg2 = await message.reply_text("Purging...", quote=True)
@@ -42,6 +45,8 @@ async def incoming_purge_message_f(client, message):
 
 async def incoming_message_f(client, message):
     """/leech command"""
+    g_id = message.from_user.id
+    credit = await message.reply_text(f"🧲 AiTorrentWare Leeching for <a href='tg://user?id={g_id}'> you </a>", parse_mode="html")
     i_m_sefg = await message.reply_text("processing", quote=True)
     is_zip = False
     is_unzip = False
@@ -86,7 +91,8 @@ async def incoming_message_f(client, message):
             cf_name,
             is_unzip,
             is_unrar,
-            is_untar
+            is_untar,
+            message
         )
         if not sagtus:
             # if FAILED, display the error message
@@ -99,6 +105,8 @@ async def incoming_message_f(client, message):
 #
 async def incoming_gdrive_message_f(client, message):
     """/gleech command"""
+    g_id = message.from_user.id
+    credit = await message.reply_text(f"🧲 AiTorrentWare Leeching for <a href='tg://user?id={g_id}'> you </a>", parse_mode="html")
     i_m_sefg = await message.reply_text("processing", quote=True)
     is_zip = False
     is_unzip = False
@@ -155,6 +163,8 @@ async def incoming_gdrive_message_f(client, message):
 
 async def incoming_youtube_dl_f(client, message):
     """ /ytdl command """
+    g_id = message.from_user.id
+    credit = await message.reply_text(f"💀 AiTorrentWare Downloading for <a href='tg://user?id={g_id}'> you </a>", parse_mode="html")
     i_m_sefg = await message.reply_text("processing", quote=True)
     # LOGGER.info(message)
     # extract link from message
@@ -162,10 +172,10 @@ async def incoming_youtube_dl_f(client, message):
         message.reply_to_message, "YTDL"
     )
     LOGGER.info(dl_url)
-    if len(message.command) > 1:
-        if message.command[1] == "gdrive":
-            with open('blame_my_knowledge.txt', 'w+') as gg:
-                gg.write("I am noob and don't know what to do that's why I have did this")
+    #if len(message.command) > 1:
+        #if message.command[1] == "gdrive":
+            #with open('blame_my_knowledge.txt', 'w+') as gg:
+                #gg.write("I am noob and don't know what to do that's why I have did this")
     LOGGER.info(cf_name)
     if dl_url is not None:
         await i_m_sefg.edit_text("extracting links")
@@ -178,14 +188,19 @@ async def incoming_youtube_dl_f(client, message):
         # list the formats, and display in button markup formats
         thumb_image, text_message, reply_markup = await extract_youtube_dl_formats(
             dl_url,
-            # cf_name,
+            cf_name,
             yt_dl_user_name,
             yt_dl_pass_word,
             user_working_dir
         )
+        print(thumb_image)
+        req = requests.get(f"{thumb_image}")
+        gau_tam = f"{current_user_id}.jpg"
+        open(gau_tam, 'wb').write(req.content)
         if thumb_image is not None:
             await message.reply_photo(
-                photo=thumb_image,
+                #text_message,
+                photo=gau_tam,
                 quote=True,
                 caption=text_message,
                 reply_markup=reply_markup
@@ -201,3 +216,36 @@ async def incoming_youtube_dl_f(client, message):
             "**FCUK**! wat have you entered. \nPlease read /help \n"
             f"<b>API Error</b>: {cf_name}"
         )
+#playlist
+async def g_yt_playlist(client, message):
+    """ /pytdl command """
+    g_id = message.from_user.id
+    #credit = await message.reply_text(f"💀 Downloading for <a href='tg://user?id={g_id}'> you </a>", parse_mode="html")
+    #i_m_sefg = await message.reply_text("Processing...you should wait🤗", quote=True)
+    usr_id = message.from_user.id
+    G_DRIVE = False
+    if len(message.command) > 1:
+        if message.command[1] == "gdrive":
+            G_DRIVE = True
+    if 'youtube.com/playlist' in message.reply_to_message.text:
+        i_m_sefg = await message.reply_text("💀 AiTorrentWare Downloading for <a href='tg://user?id={g_id}'> you </a>", parse_mode="html")
+        await yt_playlist_downg(message.reply_to_message, i_m_sefg, G_DRIVE)
+    
+    else:
+        await message.reply_text("Reply to youtube playlist link only 🙄")
+        
+ #
+async def g_clonee(client, message):
+    """ /gclone command """
+    g_id = message.from_user.id
+    if message.reply_to_message is not None:
+        LOGGER.info(message.reply_to_message.text)
+        gclone = CloneHelper(message)
+        gclone.config()
+        a, h = gclone.get_id()
+        LOGGER.info(a)
+        LOGGER.info(h)
+        await gclone.gcl()
+        await gclone.link_gen_size()
+    else:
+        await message.reply_text("You should reply to a message, which format should be [ID of Gdrive file/folder Name of the file/folder]\n\nOr check the sample, how to use this\n<a href='https://t.me/c/1278496201/18019'>cick here</a>")
